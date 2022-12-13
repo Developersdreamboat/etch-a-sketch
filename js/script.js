55a2433f96fab9d38d67347ec3744798e77c423f
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-13 22:07:06 +0200

    Add box number function and its styles

--- js/script.js
+++ js/script.js
@@ -2,31 +2,46 @@
 
 const container = document.querySelector('.container');
 let isMouseDown;
+let isCreated;
 
 function createBoxGrid(quantity) {
   let width = container.offsetWidth/quantity;
   const div = document.createElement('div');
   div.classList.add('sector');
   div.style.cssText = `width: ${width}px;`;
-  console.log(div);
-    
+  isCreated = true;
   for (let i = 0; i < quantity*quantity; i++) {    
     container.appendChild(div.cloneNode(true));
   }
 }
 
-createBoxGrid(40);
+function setUpBoxGrid(quantity, createGridCallback) {
+  const boxes = Array.from(document.querySelectorAll('.sector'));
+  if (boxes.length > 0) {
+    for (let i = 0; i < boxes.length; i++) {
+      container.removeChild(boxes[i]);
+    }
+  }
+  createGridCallback(quantity);
+}
+
+const changeButton = document.querySelector('.change-box-quantity > button');
+
+changeButton.addEventListener('click', () => {
+  let userResponse = prompt('Print a number of boxes', 16);
+  setUpBoxGrid(userResponse, createBoxGrid);
+});
 
 document.addEventListener('mousedown', () => isMouseDown = true);
 document.addEventListener('mouseup', () => isMouseDown = false);
 
 document.addEventListener("mouseover", function(e){
   const target = e.target.closest(".container");
-  console.log(isMouseDown);
   if(isMouseDown == true && 
       e.target && e.target.matches(".sector") ){
     e.target.style['background-color'] = 'white';
   }
 });
+
 
 //when hover change state, but unhover dont leave it
