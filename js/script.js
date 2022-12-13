d8cfeaa5a143f3a1f87174f15b52a042f9b4812a
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2022-12-13 19:05:10 +0200

    Add basic functionality

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 "use strict"
 
 const container = document.querySelector('.container');
+let isMouseDown;
 
-function createBox() {
-  let width = container.offsetWidth/16;
+function createBoxGrid(quantity) {
+  let width = container.offsetWidth/quantity;
   const div = document.createElement('div');
   div.classList.add('sector');
   div.style.cssText = `width: ${width}px;`;
-  console.log(width);
+  console.log(div);
     
-  for (let i = 0; i < 256; i++) {    
+  for (let i = 0; i < quantity*quantity; i++) {    
     container.appendChild(div.cloneNode(true));
-    console.log(div);
   }
 }
 
-createBox();
+createBoxGrid(40);
+
+document.addEventListener('mousedown', () => isMouseDown = true);
+document.addEventListener('mouseup', () => isMouseDown = false);
+
+document.addEventListener("mouseover", function(e){
+  const target = e.target.closest(".container");
+  console.log(isMouseDown);
+  if(isMouseDown == true && 
+      e.target && e.target.matches(".sector") ){
+    e.target.style['background-color'] = 'white';
+  }
+});
+
+//when hover change state, but unhover dont leave it
